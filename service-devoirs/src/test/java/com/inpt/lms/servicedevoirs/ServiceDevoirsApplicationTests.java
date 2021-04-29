ad4f67b048b85e02642cf3515f165e7515bbf2ca
Amine Rachyd <aminerachyd99@gmail.com>
2021-04-29 22:37:34 +0000

    Premier setup
    Setup des 2 services : Gestion de compte / Devoirs
    Setup des pipelines de test

---
+++ service-devoirs/src/test/java/com/inpt/lms/servicedevoirs/ServiceDevoirsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inpt.lms.servicedevoirs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDevoirsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-gestioncomptes/src/test/java/com/inpt/lms/servicegestioncomptes/ServiceGestioncomptesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inpt.lms.servicegestioncomptes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGestioncomptesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
