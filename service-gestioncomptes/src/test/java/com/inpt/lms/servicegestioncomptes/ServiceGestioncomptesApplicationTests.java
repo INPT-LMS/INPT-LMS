2f9fb73bde60a8baeb88b73830a5a81ababf8f11
Amine Rachyd <aminerachyd99@gmail.com>
2021-04-29 23:00:30 +0000

    Testing pipelines

--- service-gestioncomptes/src/test/java/com/inpt/lms/servicegestioncomptes/ServiceGestioncomptesApplicationTests.java
+++ service-gestioncomptes/src/test/java/com/inpt/lms/servicegestioncomptes/ServiceGestioncomptesApplicationTests.java
@@ -1,9 +1,8 @@
 package com.inpt.lms.servicegestioncomptes;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ServiceGestioncomptesApplicationTests {
 
     @Test
