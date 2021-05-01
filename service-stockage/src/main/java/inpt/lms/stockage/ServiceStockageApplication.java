62e3294fe10d55cc8b16a5a5daf2ab8acce6b363
Richard <richardotoba@gmail.com>
2021-05-01 19:34:04 +0000

    Creation du service stockage

---
+++ service-stockage/src/main/java/inpt/lms/stockage/ServiceStockageApplication.java
@@ -0,0 +1,13 @@
+package inpt.lms.stockage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStockageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStockageApplication.class, args);
+	}
+
+}
---
+++ service-stockage/src/test/java/inpt/lms/stockage/ServiceStockageApplicationTests.java
@@ -0,0 +1,13 @@
+package inpt.lms.stockage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStockageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
