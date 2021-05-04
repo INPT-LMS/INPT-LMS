238bc824ce3d11f9a819be45e573f6709df11679
wmordy <mwadii30@gmail.com>
2021-05-04 15:06:12 +0000

    initial commit for course service

---
+++ service_cours/src/main/java/com/inpt/lms/service_cours/ServiceCoursApplication.java
@@ -0,0 +1,13 @@
+package com.inpt.lms.service_cours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCoursApplication.class, args);
+	}
+
+}
