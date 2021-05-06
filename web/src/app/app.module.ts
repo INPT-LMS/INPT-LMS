83958dc311c1c39d6be84a1778a7d9e50168fdc1
Amine Rachyd <aminerachyd99@gmail.com>
2021-05-06 14:05:00 +0000

    Homepage done

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FastLinksComponent } from './components/homepage/fast-links/fast-links.component';
+import { FeedComponent } from './components/homepage/feed/feed.component';
+import { AgendaComponent } from './components/homepage/agenda/agenda.component';
+import { LinkItemComponent } from './components/homepage/fast-links/link-item/link-item.component';
+import { CommentComponent } from './components/homepage/feed/post/comment/comment.component';
+import { PostComponent } from './components/homepage/feed/post/post.component';
+import { AgendaItemComponent } from './components/homepage/agenda/agenda-item/agenda-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, FastLinksComponent, FeedComponent, AgendaComponent, LinkItemComponent, CommentComponent, PostComponent, AgendaItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
