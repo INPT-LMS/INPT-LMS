af1e9dce4cf453f45e33b34be5d34f03558c0ab8
Amine Rachyd <aminerachyd99@gmail.com>
2021-05-07 22:43:24 +0000

    Ajout de la page de cours
    Changements :
    Ajout d'un routeur Angular.
    Le chemin / fait une redirection vers /feed.
    Le chemin /course correspond à un cours.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponent } from './components/course/course.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'feed', pathMatch: 'full' },
+  { path: 'feed', component: HomepageComponent },
+  { path: 'course', component: CourseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -11,10 +11,31 @@ import { LinkItemComponent } from './components/homepage/fast-links/link-item/li
 import { CommentComponent } from './components/homepage/feed/post/comment/comment.component';
 import { PostComponent } from './components/homepage/feed/post/post.component';
 import { AgendaItemComponent } from './components/homepage/agenda/agenda-item/agenda-item.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { CourseComponent } from './components/course/course.component';
+import { CourseCoverComponent } from './components/course/course-cover/course-cover.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutCourseComponent } from './components/course/about-course/about-course.component';
+import { AddCourseComponent } from './components/course/add-course/add-course.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FastLinksComponent, FeedComponent, AgendaComponent, LinkItemComponent, CommentComponent, PostComponent, AgendaItemComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FastLinksComponent,
+    FeedComponent,
+    AgendaComponent,
+    LinkItemComponent,
+    CommentComponent,
+    PostComponent,
+    AgendaItemComponent,
+    HomepageComponent,
+    CourseComponent,
+    CourseCoverComponent,
+    AboutCourseComponent,
+    AddCourseComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
