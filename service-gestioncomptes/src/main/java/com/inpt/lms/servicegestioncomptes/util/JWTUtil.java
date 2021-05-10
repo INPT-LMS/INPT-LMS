a34ce0713b2f4f39050161d23480c0f10341aff6
Amine Rachyd <aminerachyd99@gmail.com>
2021-05-10 02:17:10 +0000

    Correction : Service comptes
    Correction du bug de JWT, ce n'etait qu'une histoire de clé qui ne se correspondaient pas.
    J'ai généré la même clé base64 pour ce service et la gateway.
    La gateway forward bien maintenant les requetes au service.

--- service-gestioncomptes/src/main/java/com/inpt/lms/servicegestioncomptes/util/JWTUtil.java
+++ service-gestioncomptes/src/main/java/com/inpt/lms/servicegestioncomptes/util/JWTUtil.java
@@ -2,14 +2,15 @@ package com.inpt.lms.servicegestioncomptes.util;
 
 import com.inpt.lms.servicegestioncomptes.model.UserInfos;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JWTUtil {
-    private String SECRET_JWT="secret";
+    private String SECRET_JWT="VnLsaJ0Z3eHhxHnGaBBJLfQaZIrk4VaawwWqL9lkCQk=";
 
     public String generateToken(UserInfos userInfos){
         return createToken(userInfos.getId());
@@ -17,6 +18,9 @@ public class JWTUtil {
 
     // TODO Vérifier date expiration - validité
     private String createToken(Long subject){
-        return Jwts.builder().claim("userId",subject).signWith(SignatureAlgorithm.HS512,SECRET_JWT.getBytes(StandardCharsets.UTF_8)).compact();
+        byte[] DEC_SECRET = Base64.getDecoder().decode(SECRET_JWT);
+        Key k = Keys.hmacShaKeyFor(DEC_SECRET);
+
+        return Jwts.builder().claim("userId",subject).signWith(k).compact();
     }
 }
