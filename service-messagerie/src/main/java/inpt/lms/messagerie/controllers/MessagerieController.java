d759070f011543e832ec2e7d115309a16a55eba9
Richard <richardotoba@gmail.com>
2021-05-12 00:19:48 +0000

    Fix messagerie
    Correction de la route GET discussion/{discId}

--- service-messagerie/src/main/java/inpt/lms/messagerie/controllers/MessagerieController.java
+++ service-messagerie/src/main/java/inpt/lms/messagerie/controllers/MessagerieController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import feign.RetryableException;
@@ -49,7 +48,8 @@ public class MessagerieController {
 	
 	@GetMapping("discussion/{discId}")
 	public Page<Message> getDiscussionMessages(@PathVariable String discId,
-			@RequestParam long userId, Pageable pagination) throws NotFoundException{
+			@RequestHeader(name = "X-USER-ID") long userId,
+			Pageable pagination) throws NotFoundException{
 		return messagerieService.getDiscussionMessages(discId,userId, pagination);
 	}
 	
