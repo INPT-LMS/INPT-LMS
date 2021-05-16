1faa1243505b51b2f84553ac4f32bf9727aff7ee
wmordy <mwadii30@gmail.com>
2021-05-16 01:04:32 +0000

    Adding posts to course page

--- lms_flutter/lib/screens/CoursePage.dart
+++ lms_flutter/lib/screens/CoursePage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:lms_flutter/course_elements/course_settings.dart';
+import 'package:lms_flutter/components/course_elements/course_settings.dart';
+import 'package:lms_flutter/components/posts/post.dart';
 import 'package:lms_flutter/screens/scaffold_app_bar.dart';
 
 class CoursePage extends StatefulWidget {
@@ -13,56 +14,54 @@ class CoursePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CoursePage> {
-
+   var posts = ["Post 1 ", "Post 2 ", "Post 3"];
   @override
   Widget build(BuildContext context) {
     return BaseScaffoldAppBar(
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
 
-        children: [
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('images/pic.jpg'),
-                        colorFilter: ColorFilter.mode(Color.fromRGBO(0, 0, 0, 0.5), BlendMode.darken),
-                        fit: BoxFit.cover),
-                  ),
-                  child: Column(
-                    children: [Align(
-
-                      child:SettingsWidget()
-                      ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text("Well this is the title",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-
-                          color: Colors.white,
-                          fontSize: 24,
-
-                        ),
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('images/pic.jpg'),
+                          colorFilter: ColorFilter.mode(Color.fromRGBO(0, 0, 0, 0.5), BlendMode.darken),
+                          fit: BoxFit.cover),
                     ),
-                    )]
-                  ),
-                  ),
+                    child: Column(
+                      children: [Align(
 
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "Your Big Text ",
-                    textAlign: TextAlign.left,
-                  ),
-                )
-              ],
+                        child:SettingsWidget()
+                        ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text("Well this is the title",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+
+                            color: Colors.white,
+                            fontSize: 24,
+
+                          ),
+                      ),
+                      )]
+                    ),
+                    ),
+
+                  Column(
+                    children: <Widget> [for(var post in posts) Post()],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       )
     );
   }
