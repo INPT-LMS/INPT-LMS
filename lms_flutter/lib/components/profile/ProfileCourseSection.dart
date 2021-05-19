c142fa7afe185d0e002a28333395148dd42334cb
wmordy <mwadii30@gmail.com>
2021-05-19 18:24:01 +0100

    edits

--- lms_flutter/lib/components/profile/ProfileCourseSection.dart
+++ lms_flutter/lib/components/profile/ProfileCourseSection.dart
@@ -11,12 +11,28 @@ class _CourseSectionState extends State<CourseSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: 14, top: 36),
-      padding: EdgeInsets.only(left: 24),
+      margin: EdgeInsets.only(
+        left: 14,
+        top: 36
+      ),
+      padding: EdgeInsets.only(
+        left: 24
+      ),
       decoration: BoxDecoration(
-          border: Border(left: BorderSide(color: Color(0xff0275B1), width: 2))),
+
+        border: Border(
+
+          left: BorderSide(
+            color: Color(0xff0275B1),
+                width: 2
+
+          )
+        )
+      ),
+
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
+
         children: [
           Text(
             "Courses",
@@ -25,20 +41,20 @@ class _CourseSectionState extends State<CourseSection> {
               color: Color(0xff0275B1),
             ),
           ),
-          for (var i = 0; i < 5; i++)
+          for(var i = 0 ; i< 5 ; i ++ )
             Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/course', (Route<dynamic> route) => false);
-                },
-                child: Text(
-                  "Course $i",
-                  style: TextStyle(fontSize: 18),
+            padding: const EdgeInsets.all(12.0),
+            child: GestureDetector(
+              onTap: () {Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/course', (Route<dynamic> route) => false);},
+              child: Text(
+                "Course ${i}",
+                style: TextStyle(
+                    fontSize: 18
                 ),
               ),
             ),
+          ),
         ],
       ),
     );
